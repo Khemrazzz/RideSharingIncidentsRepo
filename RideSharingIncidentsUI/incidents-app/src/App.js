4bb430359c25f5e60d8bb8496a2890964358fd19
Shar5K_95 <145230570+Khemrazzz@users.noreply.github.com>
2025-02-06 13:16:43 +0400

    --UI Commit
    App react

---
+++ RideSharingIncidentsUI/incidents-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FilterableTable from './HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <FilterableTable />
+    </div>
+  );
+}
+
+export default App;
